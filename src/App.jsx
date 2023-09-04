dad20832619af41031a8740e388ee7508d06e68e
Devi prasad <prasad20697@gmail.com>
2023-09-04 21:30:25 +0530

    global varibles imported in root

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import CodeEditor from "./components/CodeEditor";
+import { fonts, themes } from "./options";
+import useStore from "./store";
 
 function App() {
+	const theme = useStore((state) => state.theme);
+	const padding = useStore((state) => state.padding);
+	const fontStyle = useStore((state) => state.fontStyle);
+	const showBackground = useStore((state) => state.showBackground);
 	return (
 		<div className="dark bg-neutral-950 text-white flex flex-col gap-5 min-h-screen justify-center items-center">
 			Canva Code | Make you code look like a canvas
-      <CodeEditor />
+			<CodeEditor />
 		</div>
 	);
 }
