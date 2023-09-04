623e51ebd883c86e37965439a954701618883a03
Devi prasad <prasad20697@gmail.com>
2023-09-04 22:19:23 +0530

    handler-fn_1 init

--- src/components/controls/ExportOptions.jsx
+++ src/components/controls/ExportOptions.jsx
@@ -1,19 +1,52 @@
-import React from 'react'
-import { DropdownMenu, DropdownMenuTrigger } from '../ui/dropdown-menu'
-import { Button } from '../ui/button'
-import { Share2Icon } from '@radix-ui/react-icons'
+import React from "react";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import {
+	DownloadIcon,
+	ImageIcon,
+	Link2Icon,
+	Share2Icon,
+} from "@radix-ui/react-icons";
 
-function ExportOptions({targetRef}) {
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-         <Button>
-            <Share2Icon className="mr-2" />
-            Export
-         </Button>
-      </DropdownMenuTrigger>
-    </DropdownMenu>
-  )
+function ExportOptions({ targetRef }) {
+
+   const copyImage = async () => {}
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button>
+					<Share2Icon className="mr-2" />
+					Export
+				</Button>
+			</DropdownMenuTrigger>
+
+			<DropdownMenuContent className="dark">
+				<DropdownMenuItem className="gap-2">
+					<ImageIcon />
+					Copy Image
+				</DropdownMenuItem>
+				<DropdownMenuItem className="gap-2">
+					<Link2Icon />
+					Copy Link
+				</DropdownMenuItem>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem className="gap-2">
+					<DownloadIcon />
+					Save as PNG*
+				</DropdownMenuItem>
+				<DropdownMenuItem className="gap-2">
+					<DownloadIcon />
+					Save as SVG*
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
 }
 
-export default ExportOptions
+export default ExportOptions;
