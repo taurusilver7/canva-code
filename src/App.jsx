eb09c366e6f0af22ce0953b59b67dc6efd09e81b
Devi prasad <prasad20697@gmail.com>
2023-09-04 21:53:42 +0530

    root connected to store

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 import CodeEditor from "./components/CodeEditor";
+import { cn } from "./lib/utils";
 import { fonts, themes } from "./options";
 import useStore from "./store";
+import { useRef } from "react";
 
 function App() {
 	const theme = useStore((state) => state.theme);
 	const padding = useStore((state) => state.padding);
 	const fontStyle = useStore((state) => state.fontStyle);
 	const showBackground = useStore((state) => state.showBackground);
+
+	const editorRef = useRef(null);
 	return (
 		<div className="dark bg-neutral-950 text-white flex flex-col gap-5 min-h-screen justify-center items-center">
-			Canva Code | Make you code look like a canvas
-			<CodeEditor />
+			<link
+				rel="stylesheet"
+				href={themes[theme].theme}
+				crossOrigin="anonymous"
+			/>
+			<link
+				rel="stylesheet"
+				href={fonts[fontStyle].src}
+				crossOrigin="anonymous"
+			/>
+			<div
+				className={cn(
+					"overflow-hidden mb-2 transition-all ease-out",
+					showBackground
+						? themes[theme].background
+						: "ring ring-neutral-900"
+				)}
+				style={{ padding }}
+				ref={editorRef}
+			>
+				<CodeEditor />
+			</div>
 		</div>
 	);
 }
