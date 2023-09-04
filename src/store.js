8903bfc622e18380c01dddb6311dff07fa1f104f
Devi prasad <prasad20697@gmail.com>
2023-09-04 21:30:05 +0530

    global store init

---
+++ src/store.js
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+const useStore = create(
+	persist(
+		() => ({
+			code: "",
+			title: "Untitled",
+			theme: "hyper",
+			darkMode: true,
+			showBackground: true,
+			language: "plainText",
+			autoDetectLanguage: false,
+			fontStyle: "jetBrainsMono",
+			fontSize: 18,
+			padding: 64,
+		}),
+		{
+			name: "user-preferences",
+		}
+	)
+);
+
+export default useStore;
