4b5f0ecc118b87a56fb91f9bed5287d96e46f867
Devi prasad <prasad20697@gmail.com>
2023-09-05 18:28:52 +0530

    theme select option

--- src/components/controls/ThemeSelect.jsx
+++ src/components/controls/ThemeSelect.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "../ui/select";
+import useStore from "@/store";
+import { cn } from "@/lib/utils";
+import { themes } from "@/options";
 
 const ThemeSelect = () => {
+	const store = useStore();
+	console.log(store.theme);
 	return (
 		<div>
 			<label className="block mb-2 text-xs font-medium">Theme</label>
+			<Select
+				defaultValue={store.theme}
+				onValueChange={(theme) => useStore.setState({ theme: theme })}
+			>
+				<SelectTrigger className="w-40">
+					<SelectValue placeholder="Select Theme" />
+				</SelectTrigger>
+				<SelectContent className="dark">
+					{Object.entries(themes).map(([name, theme]) => (
+						<SelectItem key={name} value={name}>
+							<div className="flex gap-2 items-center">
+								<div
+									className={cn(
+										"h-4 w-4 rounded-full",
+										theme.background
+									)}
+								/>
+								<span className="capitalize">{name}</span>
+							</div>
+						</SelectItem>
+					))}
+				</SelectContent>
+			</Select>
 		</div>
 	);
 };
