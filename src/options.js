933df8ec84080972860b006b709ac400e9f84576
Devi prasad <prasad20697@gmail.com>
2023-09-08 21:14:58 +0530

    themes added

--- src/options.js
+++ src/options.js
@@ -51,6 +51,19 @@ export const themes = {
 		background: "bg-gradient-to-br from-green-300 via-blue-500 to-purple-600",
 		theme: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/base16/material-darker.min.css",
 	},
+	mojave: {
+		background:
+			"bg-gradient-to-tr from-yellow-100 via-yellow-300 to-yellow-500",
+		theme: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/base16/chalk.min.css",
+	},
+	seafoam: {
+		background: "bg-gradient-to-r from-green-200 via-green-300 to-blue-500",
+		theme: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/base16/chalk.min.css",
+	},
+	space: {
+		background: "bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-r",
+		theme: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/github-dark.min.css",
+	},
 	candy: {
 		background:
 			"bg-gradient-to-br from-pink-300 via-purple-300 to-indigo-400",
@@ -63,6 +76,10 @@ export const themes = {
 	},
 	horizon: {
 		background: "bg-gradient-to-br from-orange-500 to-yellow-300",
+		theme: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/monokai-sublime.min.css",
+	},
+	midnight: {
+		background: "bg-gradient-to-bl from-blue-700 via-blue-800 to-gray-900",
 		theme: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/monokai-sublime.min.css",
 	},
 	coral: {
